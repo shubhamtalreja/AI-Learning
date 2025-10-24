1ebcf05500af8f4c9b5878c6af5c8e473d5ce32f
Shubham talreja <shubhamtalreja2@gmail.com>
2025-10-24 17:16:42 +0530

    Updated the AI model.

--- Chatmodels/chatmodel_hf_api.py
+++ Chatmodels/chatmodel_hf_api.py
@@ -1,15 +1,15 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
-
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    task="text-generation"
+    repo_id="mistralai/Mistral-7B-Instruct-v0.2",
+    task="text-generation",
+
 )
 
 model = ChatHuggingFace(llm=llm)
 
-result = model.invoke("What is the capital of India")
+result = model.invoke("Diwali celebrations in India 2025")
 
 print(result.content)
